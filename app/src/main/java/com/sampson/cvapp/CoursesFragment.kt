702ac4b563f428e4d5cf4cc9f49c6a659e5aafb3
Sampson <https://fsampson30@bitbucket.org/fsampson30/trz.git>
2021-03-20 12:52:52 -0300

    Courses Fragment created.

--- app/src/main/java/com/sampson/cvapp/CoursesFragment.kt
+++ app/src/main/java/com/sampson/cvapp/CoursesFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
+import android.widget.TextView
 
 class CoursesFragment : Fragment() {
 
@@ -16,6 +18,28 @@ class CoursesFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_courses, container, false)
+        val view = inflater.inflate(R.layout.fragment_courses, container, false)
+        val txtFirstCourse = view.findViewById<TextView>(R.id.text_first_course)
+        val txtSecondCourse = view.findViewById<TextView>(R.id.text_second_course)
+        val txtThirdCourse = view.findViewById<TextView>(R.id.text_third_course)
+        val txtFourfhCourse = view.findViewById<TextView>(R.id.text_fourfh_course)
+        val btnEn = view.findViewById<ImageButton>(R.id.btnCoursesFragmentEn)
+        val btnPt = view.findViewById<ImageButton>(R.id.btnCoursesFragmentPt)
+
+        btnEn.setOnClickListener(View.OnClickListener {
+            txtFirstCourse.text = resources.getText(R.string.infnet_en)
+            txtSecondCourse.text = resources.getText(R.string.caelum_en)
+            txtThirdCourse.text = resources.getText(R.string.udemy_en)
+            txtFourfhCourse.text = resources.getText(R.string.clc_en)
+        })
+
+        btnPt.setOnClickListener(View.OnClickListener {
+            txtFirstCourse.text = resources.getText(R.string.infnet_pt)
+            txtSecondCourse.text = resources.getText(R.string.caelum_pt)
+            txtThirdCourse.text = resources.getText(R.string.udemy_pt)
+            txtFourfhCourse.text = resources.getText(R.string.clc_pt)
+        })
+
+        return view
     }
 }
